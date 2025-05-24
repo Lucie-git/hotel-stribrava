80e358416e4b725c9ed0cc19f5d6e0e543083272
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-05-24 11:15:44 +0200

    prepare basic component structure

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,21 @@
+import { Header } from '../../Components/Header/Header';
+import { Reservation } from '../../Components/Reservation/Reservation';
+import { Rooms } from '../../Components/Rooms/Rooms';
+import { Footer } from '../../Components/Footer/Footer';
+
 import './style.css';
 
 export const HomePage = () => {
   return (
-    <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
-    </div>
+    <>
+      <div className="container">
+        <main>
+          <Header />
+          <Rooms />
+          <Reservation />
+          <Footer />
+        </main>
+      </div>
+    </>
   );
 };
